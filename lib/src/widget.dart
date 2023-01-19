6bf2ac86528a02ee4837ce4c5951ee74990664e3
Sabin Neupane <61322712+sabin26@users.noreply.github.com>
2023-01-19 15:25:07 +0545

    Show borders insetead of elevation on details

--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -210,6 +210,7 @@ class _MasterDetailsFlowState extends State<MasterDetailsFlow> {
                   ),
                   child: Material(
                     shape: RoundedRectangleBorder(
+                      side: BorderSide(color: colorScheme.outline),
                       borderRadius: BorderRadius.vertical(
                         top: Radius.circular(widget.detailsPanelCornersRadius),
                       ),
@@ -219,7 +220,6 @@ class _MasterDetailsFlowState extends State<MasterDetailsFlow> {
                       colorScheme.surfaceTint,
                       selectedItem == null ? 0 : 1,
                     ),
-                    elevation: selectedItem == null ? 0 : 2,
                     clipBehavior: Clip.antiAlias,
                     child: MasterDetailsFlowSettings(
                       appBarSize: widget.lateralDetailsAppBar,
